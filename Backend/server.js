150ed0cbe7b1a5b41823552fa59acd8aa4a1c07d
Sankalp Shivhare <mrshivhare01@gmail.com>
2025-11-20 22:43:31 +0530

    hii

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001
 
 //middlewares
 app.use(cors({
-  origin: "https://fullstack-miniapp-ui.onrender.com", //linking frontend url
+  origin: "https://full-stack-miniapp.vercel.app", //linking frontend url
   credentials: true
 }));
 app.use(express.json()); //pass incoming json data
